97e9514fd961fe10c26350c758f08246e416a330
Daniel Suveges <suvi.dani@gmail.com>
2019-08-25 20:30:33 +0100

    Adding urllib3 to setup requirements.

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
     author='Daniel Suveges',
     author_email='dsuveges@ebi.ac.uk',
     description='Package that parses \'opentargets.conn.IterableResult\' objects.',
-    install_requires=['pandas', 'pytest-cov', 'opentargets', 'coverage', 'codacy-coverage']
+    install_requires=['pandas', 'pytest-cov', 'opentargets', 'urllib3', 'coverage', 'codacy-coverage']
 )
